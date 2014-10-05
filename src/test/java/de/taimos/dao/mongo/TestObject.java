cc0a55ac5dfe9e5e5e424e3d90d290bc2e362d9d
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2014-10-05 22:44:49 +0200

    serializer tests

--- src/test/java/de/taimos/dao/mongo/TestObject.java
+++ src/test/java/de/taimos/dao/mongo/TestObject.java
@@ -2,34 +2,46 @@ package de.taimos.dao.mongo;
 
 import java.math.BigDecimal;
 
+import org.joda.time.DateTime;
+
 import de.taimos.dao.AEntity;
 
 public class TestObject extends AEntity {
-	
+
 	private String name;
-	
+
 	private BigDecimal value;
+
+	private DateTime dt = DateTime.now();
 	
+	
+	public DateTime getDt() {
+		return this.dt;
+	}
+	
+	public void setDt(DateTime dt) {
+		this.dt = dt;
+	}
 	
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public BigDecimal getValue() {
 		return this.value;
 	}
-	
+
 	public void setValue(BigDecimal value) {
 		this.value = value;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "TestObject [name=" + this.name + ", value=" + this.value + ", id=" + this.id + "]";
 	}
-	
+
 }
--- src/test/java/de/taimos/dao/mongo/Tester.java
+++ src/test/java/de/taimos/dao/mongo/Tester.java
@@ -3,17 +3,28 @@ package de.taimos.dao.mongo;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
 
+import org.joda.time.DateTime;
+import org.jongo.Mapper;
+import org.jongo.marshall.jackson.JacksonMapper;
+import org.jongo.marshall.jackson.JacksonMapper.Builder;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.github.fakemongo.Fongo;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+import de.taimos.dao.JodaMapping;
 
 public class Tester {
-
+	
 	public static final TestDAO dao = new TestDAO();
-
-
+	
+	
 	@BeforeClass
 	public static void init() {
 		try {
@@ -27,7 +38,7 @@ public class Tester {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void testUpdate() {
 		TestObject o = new TestObject();
@@ -35,29 +46,58 @@ public class Tester {
 		o.setValue(new BigDecimal("5"));
 		System.out.println(o);
 		String id = o.getId();
-
+		
 		TestObject save = Tester.dao.save(o);
 		System.out.println(save);
 
 		TestObject find = Tester.dao.findById(id);
 		Assert.assertNotNull(find);
 		System.out.println(find);
-
+		
 		find.setName("blubb");
 		System.out.println(find);
-
+		
 		TestObject save2 = Tester.dao.save(find);
 		System.out.println(save2);
-		
+
 		TestObject find3 = Tester.dao.findByName("blubb");
 		Assert.assertNotNull(find3);
 		System.out.println(find3);
-
+		
 		Tester.dao.delete(id);
-
+		
 		TestObject find2 = Tester.dao.findById(id);
 		Assert.assertNull(find2);
-		
+
 	}
 
+	@Test
+	public void serialize() throws Exception {
+		TestObject o = new TestObject();
+		o.setName("bar");
+		o.setValue(new BigDecimal("5"));
+		System.out.println(o);
+
+		DBObject dbObject = this.createMapper().getMarshaller().marshall(o).toDBObject();
+		System.out.println(dbObject);
+		String json = JSON.serialize(dbObject);
+		System.out.println(json);
+
+		Object parse = JSON.parse(json);
+		System.out.println(parse);
+		System.out.println(parse.getClass());
+		System.out.println(((DBObject) parse).get("value"));
+		System.out.println(((DBObject) parse).get("value").getClass());
+	}
+
+	protected Mapper createMapper() {
+		Builder builder = new JacksonMapper.Builder();
+		builder.enable(MapperFeature.AUTO_DETECT_GETTERS);
+		builder.addSerializer(DateTime.class, new JodaMapping.MongoDateTimeSerializer());
+		builder.addDeserializer(DateTime.class, new JodaMapping.MongoDateTimeDeserializer());
+		builder.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+		builder.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+		return builder.build();
+	}
+	
 }
