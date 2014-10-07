dfe05d28122a7805297717668d4363a6c9e8eb01
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2014-10-07 08:51:32 +0200

    MongoDB Init bean

---
+++ src/main/java/de/taimos/dao/mongo/MongoDBInit.java
@@ -0,0 +1,78 @@
+package de.taimos.dao.mongo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
+
+/**
+ * Copyright 2014 Taimos GmbH<br>
+ * <br>
+ *
+ * @author Thorsten Hoeger
+ *
+ */
+public class MongoDBInit {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MongoDBInit.class);
+	
+	@Autowired
+	private MongoClient mongo;
+
+	@Value("${serviceName}")
+	private String serviceName;
+
+
+	/**
+	 * init database with demo data
+	 */
+	@PostConstruct
+	public void initDatabase() {
+		MongoDBInit.LOGGER.info("initializing MongoDB");
+		DB db = this.mongo.getDB(this.serviceName);
+		
+		try {
+			PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+			Resource[] resources = resolver.getResources("classpath*:mongodb/*.ndjson");
+			MongoDBInit.LOGGER.info("Scanning for collection data");
+			for (Resource res : resources) {
+				String filename = res.getFilename();
+				String collection = filename.substring(0, filename.length() - 7);
+				MongoDBInit.LOGGER.info("Found collection file: " + collection);
+				DBCollection dbCollection = db.getCollection(collection);
+				try (Scanner scan = new Scanner(res.getInputStream())) {
+					int lines = 0;
+					while (scan.hasNextLine()) {
+						String json = scan.nextLine();
+						Object parse = JSON.parse(json);
+						if (parse instanceof DBObject) {
+							DBObject dbObject = (DBObject) parse;
+							dbCollection.save(dbObject);
+						} else {
+							MongoDBInit.LOGGER.error("Invalid object found: " + parse);
+							throw new RuntimeException("Invalid object");
+						}
+						lines++;
+					}
+					MongoDBInit.LOGGER.info("Imported " + lines + " objects into collection " + collection);
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Error importing objects", e);
+		}
+
+	}
+}
