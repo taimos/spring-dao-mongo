86be7f65773dca5e6a555af0ebe645e83457dfb7
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2014-10-09 11:39:47 +0200

    use MongoClientURI to support ReplicaSets

--- src/main/java/de/taimos/dao/mongo/MongoClientOptionsFactory.java
+++ src/main/java/de/taimos/dao/mongo/MongoClientOptionsFactory.java
@@ -6,49 +6,49 @@ import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientOptions.Builder;
 
 /**
- * 
+ *
  * Copyright 2014 Hoegernet<br>
  * <br>
- * 
+ *
  * @author Thorsten Hoeger
- * 
+ *
  */
-public class MongoClientOptionsFactory implements FactoryBean<MongoClientOptions> {
-	
+public class MongoClientOptionsFactory implements FactoryBean<MongoClientOptions.Builder> {
+
 	private int socketTimeout;
 	private int connectTimeout;
-	
-	
+
+
 	@Override
-	public MongoClientOptions getObject() throws Exception {
+	public MongoClientOptions.Builder getObject() throws Exception {
 		Builder builder = MongoClientOptions.builder();
 		builder.socketTimeout(this.socketTimeout);
 		builder.connectTimeout(this.connectTimeout);
-		return builder.build();
+		return builder;
 	}
-	
+
 	@Override
 	public Class<?> getObjectType() {
-		return MongoClientOptions.class;
+		return MongoClientOptions.Builder.class;
 	}
-	
+
 	@Override
 	public boolean isSingleton() {
 		return false;
 	}
-	
+
 	/**
 	 * @param socketTimeout the socketTimeout to set
 	 */
 	public void setSocketTimeout(int socketTimeout) {
 		this.socketTimeout = socketTimeout;
 	}
-	
+
 	/**
 	 * @param connectTimeout the connectTimeout to set
 	 */
 	public void setConnectTimeout(int connectTimeout) {
 		this.connectTimeout = connectTimeout;
 	}
-	
+
 }
